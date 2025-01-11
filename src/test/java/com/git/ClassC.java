98b3aa5c25cccbbc78e422898c8a9d4aae2637dc
Mathimmrs123 <mathimmrs123@gmail.com>
2025-01-11 23:29:02 +0530

    added class from Mani

---
+++ src/test/java/com/git/ClassC.java
@@ -0,0 +1,15 @@
+package com.git;
+
+public class ClassC {
+	
+	private void area() {
+		// TODO Auto-generated method stub
+
+	}
+	
+	private void periyagram() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
